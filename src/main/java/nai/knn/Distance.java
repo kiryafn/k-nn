3dd9bc5a9c72b97bd75e622fd3448085ea22cb43
Kyrylo Alieksieiev <alieksieiev@Sadie-3.local>
2025-03-11 15:48:08 +0100

    Replace placeholder Main class with KNN implementation
    Removed the old `Main.java` placeholder file and added a K-nearest neighbors (KNN) implementation. Introduced `Vector`, `Distance`, and updated `Main` to include methods for calculating distances, finding nearest neighbors, and predicting results. Additionally, project configuration files were added to `.idea` for IDE setup.

---
+++ src/main/java/nai/knn/Distance.java
@@ -0,0 +1,25 @@
+package nai.knn;
+
+public class Distance {
+    private final Vector trainingVector;
+    private final Vector testVector;
+    private final double distance;
+
+    public Distance(Vector trainingVector, Vector testVector, double distance) {
+        this.trainingVector = trainingVector;
+        this.testVector = testVector;
+        this.distance = distance;
+    }
+
+    public Vector getTrainingVector() {
+        return trainingVector;
+    }
+
+    public Vector getTestVector() {
+        return testVector;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+}
---
+++ src/main/java/nai/knn/Vector.java
@@ -0,0 +1,33 @@
+package nai.knn;
+
+import java.util.List;
+
+public class Vector {
+
+    private String name;
+
+    private List<Double> values;
+
+    public Vector(String name, List<Double> values){
+        this.name = name;
+        this.values = values;
+    }
+
+    public Vector(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Double> getValues() {
+        return values;
+    }
+
+    public void setValues(List<Double> values) {
+        this.values = values;
+    }
+}
